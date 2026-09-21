8c19bf24dd1942c65f0f0ae00bd48ee738f87208
shirohito0530-code <s.hirohito0530@gmail.com>
2026-09-21 09:52:25 +0900

    Create homes.py

---
+++ scripts/adapters/homes.py
@@ -0,0 +1,14 @@
+from .base import PropertyAdapter
+
+
+class HomesAdapter(PropertyAdapter):
+
+    def __init__(self, config):
+        self.config = config
+
+    def search(self, search_config):
+        properties = []
+
+        # HOME'S用の取得処理
+
+        return properties
